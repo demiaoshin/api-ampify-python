b78a74fc63efc77f9f1bca42d64b37b722d68c9e
Demi Oshin <demiaoshin@gmail.com>
2022-01-24 18:41:34 +0000

    Completed all tasks!!

--- main.py
+++ main.py
@@ -7,17 +7,46 @@ def format_output(list):
         print(item)
 
 
+def format_song(song):
+    print(f"ID: {song['id']}\n"
+          f"Itunes ID: {song['itunesId']}\n"
+          f"Date Added: {song['dateAdded']}\n"
+          f"Name: {song['name']}\n"
+          f"Plans: {song['plans']}\n"
+          f"Genres: {song['genres']}\n"
+          f"Published?: {song['published']}\n")
+
+
 with urllib.request.urlopen("https://api.ampifymusic.com/packs") as url_json:
     data = json.loads(url_json.read().decode())
 
 songs = data['packs']
 genres = []
 all_genres = []
+hip_hop_packs = []
+count = 0
+set_of_genres = {}
+temp = []
 
 for song in songs:
     for genre in song['genres']:
         if genre in genres:
             continue
+        if genre == "hip-hop":
+            hip_hop_packs.append(song)
+            count += 1
         genres.append(genre)
 
+for song in songs:
+    for genre in genres:
+        if genre in song['genres']:
+            temp.append(song)
+        set_of_genres[genre] = temp
+
+print("LIST OF ALL GENRES:")
 format_output(genres)
+
+print("LIST OF ALL PACKS WITH THE GENRE 'HIP-HOP':")
+# There is only one song that has the genre hip hop so we print out the only song here
+format_song(hip_hop_packs[0])
+
